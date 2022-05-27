8fe3562a4bfd2cfdfe9b8cb368eccb0cf645772c
“mjgomezmora” <mjmora@gmail.com>
2022-05-27 22:00:49 +0000

    Use one json file for kubernetes deploy

--- blue/run_kubernetes.sh
+++ blue/run_kubernetes.sh
@@ -1,5 +1,2 @@
 # Run the Docker Hub container with kubernetes
-kubectl apply -f ./blue-controller.json
-
-# Create service LoadBalancer blue
-kubectl apply -f ./blue-service.json
+kubectl apply -f ./blue-controller-service.json
--- green/run_kubernetes.sh
+++ green/run_kubernetes.sh
@@ -1,5 +1,2 @@
 # Run the Docker Hub container with kubernetes
-kubectl apply -f ./green-controller.json
-
-# Create service LoadBalancer blue
-kubectl apply -f ./blue-service.json
+kubectl apply -f ./green-controller-service.json
